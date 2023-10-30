facc9c2046d7806905b6c76d96a876a6ed371912
yash-sangwan <yashsangwan00@gmail.com>
2023-10-30 14:08:59 +0530

    https://github.com/yash-sangwan/Library_AI_Zone.git

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,176 @@
+import React from "react";
+import { Box, Typography, Card, Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import DescriptionRounded from "@mui/icons-material/DescriptionRounded";
+import FormatAlignLeftOutlined from "@mui/icons-material/FormatAlignLeftOutlined";
+import ChatRounded from "@mui/icons-material/ChatRounded";
+const Homepage = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Box sx={{ display: "flex", flexDirection: "row" }}>
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+          Summary Generation
+          </Typography>
+          <Card
+            onClick={() => navigate("/summary")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <DescriptionRounded
+              sx={{ fontSize: 80, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={3} pt={0}>
+              <Typography fontWeight="bold" variant="h5">
+              STORY SUMAMRY
+              </Typography>
+              <Typography variant="h6">
+              Summarize Long Stories Into Short.
+              </Typography>
+            </Stack>
+          </Card>
+        </Box>
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+          Story Generation
+          </Typography>
+          <Card
+            onClick={() => navigate("/paragraph")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <FormatAlignLeftOutlined
+              sx={{ fontSize: 80, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={3} pt={0}>
+              <Typography fontWeight="bold" variant="h5">
+              BOOK STORY
+              </Typography>
+              <Typography variant="h6">
+              Generate Story With Book Name
+              </Typography>
+            </Stack>
+          </Card>
+        </Box>
+
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+          Book Images
+          </Typography>
+          <Card
+            onClick={() => navigate("/scifi-image")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <ChatRounded
+              sx={{ fontSize: 80, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={3} pt={0}>
+              <Typography fontWeight="bold" variant="h5">
+              BOOK IMAGE
+              </Typography>
+              <Typography variant="h6">Generate Your Imaginary Book Image</Typography>
+            </Stack>
+          </Card>
+        </Box>
+
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+          Librarian
+          </Typography>
+          <Card
+            onClick={() => navigate("/chatbot")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <ChatRounded
+              sx={{ fontSize: 80, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={3} pt={0}>
+              <Typography fontWeight="bold" variant="h5">
+              Mr. Bot
+              </Typography>
+              <Typography variant="h6">Chat With Librarian</Typography>
+            </Stack>
+          </Card>
+        </Box>
+        <Box p={2}>
+          <Typography variant="h4" mb={2} fontWeight="bold">
+            Javascript Converter
+          </Typography>
+          <Card
+            onClick={() => navigate("/js-converter")}
+            sx={{
+              boxShadow: 2,
+              borderRadius: 5,
+              height: 190,
+              width: 200,
+              "&:hover": {
+                border: 2,
+                boxShadow: 0,
+                borderColor: "primary.dark",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <ChatRounded
+              sx={{ fontSize: 80, color: "primary.main", mt: 2, ml: 2 }}
+            />
+            <Stack p={3} pt={0}>
+              <Typography fontWeight="bold" variant="h5">
+                JS CONVERTER
+              </Typography>
+              <Typography variant="h6">
+                Trasnlate english to javascript code
+              </Typography>
+            </Stack>
+          </Card>
+        </Box>
+        
+      </Box>
+    </>
+  );
+};
+
+export default Homepage;
